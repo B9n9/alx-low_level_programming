f6e387b93ddb7b8766904f908fd5e45dc11b7c26
B9n9 <biniterefe12@gmail.com>
2023-09-20 13:04:11 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-main.c
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -26,7 +26,18 @@ int main(void)
         }
         n = "1234567890";
         m = "1";
-	res = infinite_add(n, m, r2, 10);
+        res = infinite_add(n, m, r2, 10);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        n = "999999999";
+        m = "1";
+        res = infinite_add(n, m, r2, 10);
         if (res == 0)
         {
                 printf("Error\n");
